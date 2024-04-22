e71b18283adbe211bc1a8d141c88cb20d7b67176
julord87 <julianmzg@gmail.com>
2024-04-22 18:27:58 -0300

    backend auth ready

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-import { hashPassword } from "../utils/auth";
+import { checkPassword, hashPassword } from "../utils/auth";
 import Token from "../models/Token";
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../emails/AuthEmail";
@@ -39,6 +39,67 @@ export class AuthController {
 
             await Promise.allSettled([user.save(), token.save()])
             res.send('Cuenta creada correctamente, revisa tu email para confirmarla')
+        } catch (error) {
+            res.status(500).json({error: 'Hubo un error'})
+        }
+    }
+
+    static confirmAccount = async (req : Request, res : Response) => {
+        try {
+            const {token} = req.body
+            
+            const tokenExist = await Token.findOne({token})
+            if(!tokenExist) {
+                const error = new Error('Token no válido')
+                return res.status(404).json({error : error.message})
+            }
+
+            const user = await User.findById(tokenExist.user)
+            user.confirmed = true
+
+            await Promise.allSettled([user.save(), tokenExist.deleteOne()])
+            res.send('Cuenta confirmada correctamente')
+        } catch (error) {
+            res.status(500).json({error: 'Hubo un error'})
+        }
+    }
+
+    static login = async (req : Request, res : Response) => {
+        try {
+            const {email, password} = req.body
+            const user = await User.findOne({email})
+
+            if(!user) {
+                const error = new Error('El usuario no existe')
+                return res.status(404).json({error : error.message})
+            }
+
+            if(!user.confirmed) {
+                const token = new Token()
+                token.user = user.id
+                token.token = generateToken()
+                await token.save()
+
+                // Enviar el email
+                AuthEmail.sendConfirmationEmail({
+                    email: user.email,
+                    name: user.name,
+                    token: token.token,
+                }) 
+
+                const error = new Error('Tu cuenta no ha sido confirmada, revisa tu correo electrónico.')
+                return res.status(401).json({error : error.message})
+            }
+
+            // Revisar password
+            const isPasswordCorrect = await checkPassword(password, user.password)
+
+            if(!isPasswordCorrect) {
+                const error = new Error('El password es incorrecto')
+                return res.status(401).json({error : error.message})
+            }
+
+            res.send('Login correcto')
         } catch (error) {
             res.status(500).json({error: 'Hubo un error'})
         }
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -14,10 +14,26 @@ router.post("/create-account",
         }
         return true
     }),
-    body('password').notEmpty().isLength({min: 6}).withMessage("El password es obligatoria"),
+    body('password').notEmpty().isLength({min: 6}).withMessage("El password es obligatorio"),
     handleInputErrors,
 
     AuthController.createAccount
+)
+
+router.post("/confirm-account",
+    body('token').notEmpty().withMessage("Token requerido"),
+    handleInputErrors,
+
+    AuthController.confirmAccount
+)
+
+router.post("/login",
+    body('email').notEmpty().withMessage("El correo es obligatorio").isEmail().withMessage("El correo no es valido"),
+    body('password').notEmpty().withMessage("El password es obligatorio"),
+
+    handleInputErrors,
+
+    AuthController.login
 )
 
 export default router
