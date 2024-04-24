8294cbbfb29407b3394d7790eed68bb87a6074b2
julord87 <julianmzg@gmail.com>
2024-04-24 06:55:43 -0300

    auth done

--- frontend/src/api/AuthAPI.ts
+++ frontend/src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { ConfirmToken, ForgotPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
+import { ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
 
 
 export async function createAccount(formData: UserRegistrationForm) {
@@ -54,6 +54,30 @@ export async function authenticateUser(formData: UserLoginForm) {
 export async function resetPassword(formData: ForgotPasswordForm) {
     try {
         const url = '/auth/forgot-password'
+        const {data} = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function validateToken(formData: ConfirmToken) {
+    try {
+        const url = '/auth/validate-token'
+        const {data} = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function updatePasswordWithToken({formData, token}: {formData: NewPasswordForm, token: ConfirmToken['token']}) {
+    try {
+        const url = `/auth/update-password/${token}`
         const {data} = await api.post<string>(url, formData)
         return data
     } catch (error) {
--- frontend/src/views/auth/NewPasswordView.tsx
+++ frontend/src/views/auth/NewPasswordView.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import NewPasswordToken from "@/components/auth/NewPasswordToken"
 import NewPasswordForm from "@/components/auth/NewPasswordForm"
+import { ConfirmToken } from "@/types/index"
 
 
 export default function NewPasswordView() {
+  const [token, setToken] = useState<ConfirmToken['token']>('')
   const [isValidToken, setIsValidToken] = useState(false)
 
   return (
@@ -14,7 +16,9 @@ export default function NewPasswordView() {
         <span className=" text-fuchsia-500 font-bold"> a tu correo</span>
       </p>
 
-      {!isValidToken ? <NewPasswordToken /> : <NewPasswordForm />}
+      {!isValidToken ? 
+        <NewPasswordToken token={token} setToken={setToken} setIsValidToken={setIsValidToken} /> : 
+        <NewPasswordForm token={token}/>}
     </>
 
 
